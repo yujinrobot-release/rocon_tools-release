66c272686f9608d2efe68b326ee9f7980b717daa
Daniel Stonier <d.stonier@gmail.com>
2014-03-31 07:45:46 +0900

    Imported upstream version '0.1.0' of 'upstream'

---
+++ rocon_master_info/src/rocon_master_info/__init__.py
@@ -0,0 +1,10 @@
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_tools/license/LICENSE
+#
+##############################################################################
+# Imports
+##############################################################################
+
+from .master import RoconMaster
+from .master_info import main
