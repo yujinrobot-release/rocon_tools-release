2c4ab537a2cfb65ee73963b0319d53bbb2e006b6
Daniel Stonier <d.stonier@gmail.com>
2014-04-02 17:50:48 +0900

    Revert "Trimmed the branch to only the rocon_console sub directory"
    This reverts commit b8d856263f1dc46cdcaea2cef3feeda35d270b82.

---
+++ rocon_python_utils/src/rocon_python_utils/exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_tools/license/LICENSE
+#
+##############################################################################
+# Exceptions
+##############################################################################
+
+
+class TimeoutExpiredError(Exception):
+    pass
